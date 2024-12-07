3a368ea33df7f4af6218de2d12d0f5ae8d09e242
Laura González <alu0101203942@ull.edu.es>
2024-12-07 17:32:30 +0000

    Constructor de la clase Match

--- src/main/java/app/Match.java
+++ src/main/java/app/Match.java
@@ -1,4 +1,23 @@
 package app;
 
 public class Match {
+
+    private String homeTeam;
+    private String awayTeam;
+    private int homeGoals;
+    private int awayGoals;
+    private String matchId;
+    private boolean isFinished; // Nuevo campo para almacenar si el partido ha finalizado
+    private String changeMessage;
+
+    public Match(String homeTeam, String awayTeam, int homeGoals, int awayGoals, String homeLogo, String awayLogo, String matchId, int time, boolean isFinished) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeGoals = homeGoals;
+        this.awayGoals = awayGoals;
+        this.matchId = matchId;
+        this.isFinished = isFinished; // Inicializar estado
+        this.changeMessage = ""; // Inicialización vacía
+
+    }
 }
