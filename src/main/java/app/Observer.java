8ecbf756277bc09c27e9de64860f750542ac54a0
eglezgut03 <alu0101461588@ull.edu.es>
2024-12-10 21:36:04 +0000

    Comentarios Observer.java

--- src/main/java/app/Observer.java
+++ src/main/java/app/Observer.java
@@ -1,6 +1,8 @@
 package app;
 
-//Interfaz para los Observadores
+/**
+ * Observer interface for the Observer pattern.
+ */
 public interface Observer {
-    void update(Match match);
+  void update(Match match); // Update method to be called by the Subject
 }
