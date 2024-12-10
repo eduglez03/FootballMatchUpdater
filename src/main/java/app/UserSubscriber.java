5c5cb79c9828b7b1f439de7a1638325bc7816dbb
eglezgut03 <alu0101461588@ull.edu.es>
2024-12-10 21:38:23 +0000

    Comentarios UserSubscriber.java

--- src/main/java/app/UserSubscriber.java
+++ src/main/java/app/UserSubscriber.java
@@ -3,30 +3,44 @@ package app;
 import java.util.Map;
 import javax.swing.JTextArea;
 
+/**
+ * Abstract class that represents a user subscriber to match notifications.
+ */
 public abstract class UserSubscriber implements Observer {
-    private String username;
-    private Map<String, JTextArea> userNotificationAreas;
+    private String username; // Username of the subscriber
+    private Map<String, JTextArea> userNotificationAreas; // Map of notification areas for each user
 
+    /**
+     * Constructor that initializes the user subscriber with a username and a map of notification areas.
+     * @param username Username of the subscriber
+     * @param userNotificationAreas Map of notification areas for each user
+     */
     public UserSubscriber(String username, Map<String, JTextArea> userNotificationAreas) {
         this.username = username;
         this.userNotificationAreas = userNotificationAreas;
     }
 
+    /**
+     * Method to update the user subscriber with a new match.
+     * @param match Match to update the user subscriber with
+     */
     @Override
     public void update(Match match) {
-        // Obtener el área de notificaciones asociada a este usuario
+        // Get the notification area for the user
         JTextArea notificationArea = userNotificationAreas.get(username);
         if (notificationArea != null) {
-            // Agregar mensaje de cambio al área de notificación
+            // Append the change message to the notification area
             notificationArea.append(match.getChangeMessage() + "\n");
         } else {
             System.err.println("No se encontró el área de notificaciones para el usuario: " + username);
         }
     }
 
-    public String getUsername() {
-        return username;
-    }
+    /**
+     * Getter for the username of the user subscriber.
+     * @return Username of the user subscriber
+     */
+    public String getUsername() { return username; }
 }
 
 
